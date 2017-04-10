09d8279c5c2ae202de992e1ce5a16235a850cc75
janek <orlinskj@linux.pl>
2017-04-10 20:14:45 +0200

    Solver improved (all equations implemented)

--- src/model/solver.cpp
+++ src/model/solver.cpp
@@ -1,43 +1,131 @@
 #include "solver.h"
 
-SolverType BVDSolver::Co() const
+//#include <boost/math/constants/constants.hpp>
+
+bool BVDSolver::solve(const Transducer& t, boost::optional<SolverType> input_capacity)
 {
-    return Co_;
+    // getting frequency set
+    auto freq_unit = Unit::from_symbol("f");
+    auto freq_set = t.get_set(freq_unit);
+    if (!freq_set)
+        throw std::runtime_error("Transducer does not contain frequency data set.");
+
+    // getting impedance set
+    auto impedance_unit = Unit::from_symbol("|Z|");
+    auto impedance_set = t.get_set(impedance_unit);
+    if (!impedance_set)
+        throw std::runtime_error("Transducer does not contain impedance magnitude data set.");
+
+    // getting impedance imaginary set
+    auto imaginary_unit = Unit::from_symbol("Im");
+    auto imaginary_set = t.get_set(imaginary_unit);
+    if (!imaginary_set)
+        throw std::runtime_error("Transducer does not contain impedance imaginary fraction data set.");
+
+    // calculating resonance frequencies
+    // looking for min and max impedance - min is for series, max is for parallel resonance
+    SolverType fs_index = std::get<1>(impedance_set->max());
+    SolverType fp_index = std::get<1>(impedance_set->min());
+
+    //SolverType pi = boost::math::constants::pi<SolverType>();
+    SolverType pi = 3.14159;
+
+    SolverType fs = freq_set->values().at(fs_index);
+    SolverType fp = freq_set->values().at(fp_index);
+    SolverType ws = 2 * pi * fs;
+    SolverType wp = 2 * pi * fp;
+
+    // calculating transducer input capacity
+    if (input_capacity)
+        Cop_ = *input_capacity;
+    else
+    {
+        SolverType min_f = freq_set->values().at(0);
+        SolverType min_f_Im = imaginary_set->values().at(0);
+        SolverType min_w = 2.0 * M_PI * min_f;
+
+        Cop_ = 1.0 / (min_w * min_f_Im);
+    }
+
+    SolverType f_ratio = fs_ / fp_;
+    Cos_ = f_ratio*f_ratio * Cop_;
+
+    // calculating R
+    Rs_ = impedance_set->values().at(fs_index);
+    Rp_ = impedance_set->values().at(fp_index);
+
+    // calculating C
+    Cs_ = Cos_ * (fp_*fp_-fs_*fs_)/(fs_*fs_);
+    Cp_ = Cop_ * (fp_*fp_)/(fp_*fp_-fs_*fs_);
+
+    // calculating L
+    Ls_ = 1.0 / (ws*ws*Cs_);
+    Lp_ = 1.0 / (wp*wp*Cp_);
+
+    // calculating Q
+    Qs_ = 1.0 / Rs_ * std::sqrt(Ls_/Cs_);
+    Qp_ = Rp_ * std::sqrt(Cp_/Lp_);
+    QQ_ = std::sqrt(Qs_*Qp_);
+
+    // calculating coefficients
+    //SolverType half_pi = boost::math::constants::half_pi<SolverType>();
+    SolverType half_pi = 1.570796;
+    k33_ = std::sqrt(half_pi*fs_*fp_*std::tan((fp_-fs_)/fp_*half_pi));
+    keff_ = std::sqrt((fp_*fp_-fs_*fs_)/(fs_*fs_));
+    k_ = (keff_/k33_)*(keff_/k33_);
+
+    return true;
 }
 
-SolverType BVDSolver::R() const
+SolverType BVDSolver::Co(BVDSolver::Type type) const
 {
-    return R_;
+    if (type == BVDSolver::Type::Series)
+        return Cos_;
+    else
+        return Cop_;
 }
 
-SolverType BVDSolver::L() const
+SolverType BVDSolver::R(BVDSolver::Type type) const
 {
-    return L_;
+    if (type == BVDSolver::Type::Series)
+        return Rs_;
+    else
+        return Rp_;
 }
 
-SolverType BVDSolver::C() const
+SolverType BVDSolver::L(BVDSolver::Type type) const
 {
-    return C_;
+    if (type == BVDSolver::Type::Series)
+        return Ls_;
+    else
+        return Lp_;
 }
 
-BVDSeriesSolver::BVDSeriesSolver(const Transducer& t)
+SolverType BVDSolver::C(BVDSolver::Type type) const
 {
-    this->solve(t);
+    if (type == BVDSolver::Type::Series)
+        return Cs_;
+    else
+        return Cp_;
 }
 
-bool BVDSeriesSolver::solve(const Transducer &t)
+SolverType BVDSolver::f(BVDSolver::Type type) const
 {
-    SolverType Co, R, L, C;
-    return false;
+    if (type == BVDSolver::Type::Series)
+        return fs_;
+    else
+        return fp_;
 }
 
-BVDParallelSolver::BVDParallelSolver(const Transducer& t)
+SolverType BVDSolver::Q(BVDSolver::Type type) const
 {
-    this->solve(t);
+    if (type == BVDSolver::Type::Series)
+        return Qs_;
+    else
+        return Qp_;
 }
 
-bool BVDParallelSolver::solve(const Transducer &t)
-{
-    SolverType Co, R, L, C;
-    return false;
-}
+SolverType BVDSolver::QQ() const { return QQ_; }
+SolverType BVDSolver::keff() const { return keff_; }
+SolverType BVDSolver::k() const { return k_; }
+SolverType BVDSolver::k33() const { return k33_; }
--- src/model/solver.h
+++ src/model/solver.h
@@ -7,25 +7,36 @@
 
 using SolverType = double;
 
-class Solver
+class BVDSolver
 {
 public:
-    virtual bool solve(const Transducer& t) = 0;
-};
+    enum class Type{
+        Series,
+        Parallel
+    };
 
-class BVDSolver : public Solver
-{
-public:
-    bool solve(const Transducer& t);
-    bool solve(const Transducer& t, boost::optional<SolverType> Co);
+    bool solve(const Transducer& t, boost::optional<SolverType> input_capacity);
 
-    SolverType Co() const;
-    SolverType R() const;
-    SolverType L() const;
-    SolverType C() const;
+    SolverType Co(Type type) const;
+    SolverType R(Type type) const;
+    SolverType L(Type type) const;
+    SolverType C(Type type) const;
+    SolverType Q(Type type) const;
+    SolverType f(Type type) const;
+
+    SolverType QQ() const;
+    SolverType k33() const;
+    SolverType keff() const;
+    SolverType k() const;
+
 protected:
-    SolverType Co_, R_, L_, C_;
-
+    SolverType Cos_, Cop_;
+    SolverType Rs_, Rp_;
+    SolverType Ls_, Lp_;
+    SolverType Cs_, Cp_;
+    SolverType fs_, fp_;
+    SolverType Qs_, Qp_, QQ_;
+    SolverType keff_, k33_, k_;
 
 };
 
